93c96accd944836c8dc569aff44b82e6221b3393
Juan Diego <juandiego@um.es>
2025-06-04 21:14:05 +0200

    Muchisimos cambios

--- src/main/java/com/dam/checkinn/configurations/Security.java
+++ src/main/java/com/dam/checkinn/configurations/Security.java
@@ -3,59 +3,63 @@ package com.dam.checkinn.configurations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class Security {
 
     @Bean
-
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())    // Deshabilita CSRF para APIs REST
-
-    // TODO ACTIVAR ESTO DESPUES
-    //                .authorizeHttpRequests(auth -> auth
-    //                        .requestMatchers(
-    //                                "/api/v1/login",
-    //                                "/api/v1/usuarios/register").permitAll()  // acceso libre a login
-    //                        .anyRequest().authenticated()
-    //                )
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // 🔓 Permite TODO el acceso
-                )
-                .formLogin(form -> form.disable());  // Deshabilita login automático
-
-        return http.build();
-    }
-
 //    @Bean
-//    @Order(3)
 //    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 //        http
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(authorizeRequests -> {authorizeRequests
-//                        .requestMatchers("/api/v1/login").permitAll()
-////                        .requestMatchers("/Index.html").permitAll()
-////                        .requestMatchers("").permitAll()
-//                        .anyRequest().authenticated();
+//                .csrf(csrf -> csrf.disable())    // Deshabilita CSRF para APIs REST
 //
-//                }).formLogin(form -> {
-//                    form.loginPage("/Index")
-//                            .defaultSuccessUrl("/Index", true)
-//                            .permitAll();
-//                });
-//                return http.build();
+//    // TODO ACTIVAR ESTO DESPUES
+//    //                .authorizeHttpRequests(auth -> auth
+//    //                        .requestMatchers(
+//    //                                "/api/v1/login",
+//    //                                "/api/v1/usuarios/register").permitAll()  // acceso libre a login
+//    //                        .anyRequest().authenticated()
+//    //                )
+//                .authorizeHttpRequests(auth -> auth
+//                        .anyRequest().permitAll() // 🔓 Permite TODO el acceso
+//                )
+//                .formLogin(form -> form.disable());  // Deshabilita login automático
+//
+//        return http.build();
 //    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests( request -> {request
+                        .requestMatchers("/Index.html").permitAll()
+                        .requestMatchers("/Registro.html").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/usuarios").permitAll()
+                        .requestMatchers("app.js", "/images/**").permitAll()
+
+                        .anyRequest().authenticated();
+        })
+//                .formLogin(form -> form
+//                        .loginPage("/Index.html")               // Ruta a tu formulario personalizado
+//                        .loginProcessingUrl("/api/v1/login")   // Donde se enviará el formulario (POST)
+//                        .defaultSuccessUrl("/Home.html", true)  // Página a la que redirige si el login es exitoso
+//                        .failureUrl("/api/v1/login")   // Página si el login falla
+//                        .permitAll()
+//                )
+                .csrf( csrf -> csrf.disable());
+        return http.build();
+    }
 }
 
